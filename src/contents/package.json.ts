2199e228d056487d0dc000c7bc40b229ef49b2d7
damartripamungkas <damartripamungkas@github.com>
2024-08-14 18:49:07 +0700

    fix: remove command for content package.json

--- src/contents/package.json.ts
+++ src/contents/package.json.ts
@@ -2,7 +2,7 @@ import { TypeParamsContent } from "../type"
 
 export default {
   filename: `package.json`,
-  command: `npm i @types/node --force --save-dev`,
+  command: ``,
   content: ({ previousContent, projectName }: TypeParamsContent) => {
     return JSON.stringify(
       {
