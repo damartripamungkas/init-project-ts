3555d85f9dca641b4d344d0bb04cb30d8873f627
damartripamungkas <damartripamungkas@gmail.com>
2024-11-12 11:42:03 +0700

    chore: add config file for runtime node in contents/test.index.ts

--- src/contents/test.index.ts
+++ src/contents/test.index.ts
@@ -1,17 +1,19 @@
-import { getState } from "../libs/state"
+import { writeFileSync } from "node:fs"
 import { TypeParamsContent } from "../type"
+import state from "../libs/state"
+import { join } from "node:path"
 
 export default {
   filename: `test/index.test.ts`,
   command: () => {
-    const projectRuntime = getState(`projectRuntime`)
+    const { projectRuntime } = state
     if (projectRuntime == `bun`) {
       return ``
     }
     return `npm i vitest --force --save-dev`
   },
   content: ({ previousContent }: TypeParamsContent) => {
-    const projectRuntime = getState(`projectRuntime`)
+    const { pathRoot, projectRuntime } = state
     if (projectRuntime == `bun`) {
       return `import { describe, it, expect } from "bun:test"
 import contextDummy from "../src/index"
@@ -24,6 +26,35 @@ describe("test file index.ts", () => {
 })`
     }
 
+    const fileConfigTest = `import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  test: {
+    globals: true, // Mengaktifkan global test functions (describe, it, etc.)
+    include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], // Pola file yang akan diuji
+    exclude: ["node_modules/", "dist/", "coverage/"], // File/direktori yang dikecualikan dari pengujian
+    environment: "node", // Lingkungan pengujian (node, jsdom, happy-dom, etc.)
+    setupFiles: [], // File konfigurasi tambahan yang akan dimuat sebelum pengujian
+    clearMocks: true, // Membersihkan mock/spy setelah setiap tes
+    coverage: {
+      enabled: false, // Mengaktifkan pengukuran coverage
+      provider: "istanbul", // Provider coverage yang digunakan (istanbul, c8, etc.)
+      reporter: "json" // Format laporan coverage
+    },
+    restoreMocks: true, // Mengembalikan mock/spy ke state awal setelah tes
+    mockReset: true, // Reset mock/spy ke state awal setelah tes
+    maxConcurrency: 5, // Jumlah maksimal konkurensi yang digunakan
+    silent: false, // Menyembunyikan log output
+    watch: true, // Mengaktifkan mode watch
+    logHeapUsage: true, // Mencatat penggunaan memori
+    testTimeout: 0, // Batas waktu tes (dalam millisecond)
+    hookTimeout: 10000, // Batas waktu hook (dalam millisecond)
+    testNamePattern: "" // Filter tes berdasarkan nama
+  }
+})
+`
+
+    writeFileSync(join(pathRoot, `vitest.config.mts`), fileConfigTest, { encoding: `utf8` })
     return `import { describe, expect, test } from "vitest"
 import contextDummy from "../src/index"
 
