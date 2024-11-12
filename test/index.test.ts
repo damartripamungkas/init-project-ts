f09d3587b9d062b558932419ccf3be175a1ef27b
damartripamungkas <damartripamungkas@gmail.com>
2024-11-12 11:36:03 +0700

    test: remove file test with unit test

--- test/index.test.ts
+++
@@ -1,9 +0,0 @@
-import { basename } from "node:path"
-import { describe, expect, test } from "vitest"
-const base = basename(process.cwd())
-
-describe(`test file index.ts`, () => {
-  test(`is position shell in folder test?`, async () => {
-    expect(base).toBe(`test`)
-  })
-})
